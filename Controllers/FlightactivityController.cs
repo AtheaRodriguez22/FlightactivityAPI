60b2b4e00003342df819e4017ff6405c3e55d643
AtheaRodriguez22 <rodriguezatheairish22@gmail.com>
2026-06-07 20:02:35 +0800

    Add project files.

---
+++ Controllers/FlightactivityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightactivityAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightactivityController : ControllerBase
+    {
+    }
+}
